a7edecbdffb1e2708d2a39b76433e3a4e59271ad
RemyBen <pntdu83@gmail.com>
2020-09-02 11:54:25 +0200

    Add conf, define, parametres, index, gestionclientcontroler, identificationcontroller, gestionclientmodel

--- config/define.php
+++ config/define.php
@@ -1,8 +1,8 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// define path
+define('PATH_CSS', RACINE. "assets".DS."css.".DS);
+define('PATH_JS', RACINE. "assets".DS."js.".DS);
+define('PATH_VENDOR', RACINE. "vendor".DS);
+define('PATH_VIEW', RACINE . "template".DS);
 
--- parametres.php
+++ parametres.php
@@ -6,3 +6,8 @@
  * and open the template in the editor.
  */
 
+define('DATABASE_URL', "localhost");
+define('DATABASE_USER', "benalouane");
+define('DATABASE_PWD', 'PnT645645!');
+define('DATABASE_NAME', "clicom");
+define('CNSTRING', "mysql:host=" . DATABASE_URL . ";dbname=" . DATABASE_NAME . ";charset=UTF8");
