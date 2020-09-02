79f9d21a0ecea56a2fa28b55ffe299ab8033440b
RemyBen <pntdu83@gmail.com>
2020-09-02 10:06:03 +0200

    initial project

---
+++ includes/Connexion.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Connexion
+ *
+ * @author remy.benalouane
+ */
+class Connexion {
+
+    private static $connexion = null;
+    private static $connexionInstance = null;
+
+    private function __construct() {
+        try {
+            $connect_str = CNSTRING;
+            $connect_user = DATABASE_USER;
+            $connect_pass = DATABASE_PWD;
+            $options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+            self::$connexion = new PDO($connect_str, $connect_user, $connect_pass, $options);
+        } catch (PDOException $e) {
+            throw new Exception('Erreur à la connexion <br>' . $e->getMessage());
+        }
+    }
+
+   
+    public static function getConnexion() {
+
+        if (is_null(self::$connexionInstance)) {
+            self::$connexionInstance = new Connexion();
+        }
+
+        return self::$connexion;
+    } 
